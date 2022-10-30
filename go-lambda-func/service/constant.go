4113223c7428793983919118daa288afbd6e932a
Pratyush Harsh <pratyushharsh2015@gmail.com>
2022-10-30 21:53:52 +0530

    Minor Chages and Bug Fixes

--- go-lambda-func/service/constant.go
+++
@@ -1,8 +0,0 @@
-package src
-
-import "os"
-
-var CommonTable string = os.Getenv("DBTable")
-
-const CognitoUserPool string = "ap-south-1_gXgaeT7lu"
-const XPOSSecondaryIndex string = "GPK1-GSK1-index"
